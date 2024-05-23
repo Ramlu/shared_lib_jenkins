62f497afd9bd53c5bb9b9510b27364ca82544d7d
Ramlu <naveenramlu@gmail.com>
2024-05-23 15:40:17 +0530

    Credentials-id has added

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,8 +1,25 @@
 def call(Map stageParams){
 
+    // checkout([
+    //     $class: 'GitSCM',
+    //     branches: [[ name: stageParams.branch ]],
+    //     //userRemoteConfigs: [[ url: stageParams.url ]]
+
+    //      userRemoteConfigs: [[
+    //         url: stageParams.url,
+    //         credentialsId: stageParams.credentialsId
+    //     ]]
+    // ])
+    Map userremoteConfig = [url: stageParams.url]
+    
+    if (stageParams.credentialsId) {
+        userremoteConfig.credentialsId = stageParams.credentialsId
+    }
+    
     checkout([
         $class: 'GitSCM',
-        branches: [[ name: stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
+        branches: [[name: stageParams.branch]],
+        userRemoteConfigs: [userremoteConfig]
     ])
+
 }
